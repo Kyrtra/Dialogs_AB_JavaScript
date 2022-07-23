513e0c34cb862161f8dc2c145646a001a30c8817
Kyrtra <ilze_senk@inbox.lv>
2022-07-23 17:57:31 +0300

    class_19_hometask

---
+++ Class_20_2022/expense_tracker/expense_tracker.js
@@ -0,0 +1,129 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
+const current_day = new Date;
+const datuma_teksts = current_day.toLocaleString('lv-LV', {timeZone: 'Europe/Riga'});
+
+var today = new Date();
+var day = today.getDay();
+var day_list = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+document.getElementById("current_date").innerHTML = day_list[day]+",  " +  datuma_teksts;
+
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `EUR ${total}`;
+  money_plus.innerText = `EUR ${income}`;
+  money_minus.innerText = `EUR ${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
---
+++ Useful_links.md
@@ -0,0 +1,17 @@
+# JS_links
+<h2>Several useful JS links</h2>
+
+1. [Mozilla Developer Network (MDN)](https://developer.mozilla.org/en-US/);
+2. [Saskarnes no MDN](https://developer.mozilla.org/en-US/docs/Web/API);
+3. [JavaScript programmēšanas valodas sintakses elementi no MDNURL](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference);
+4. [JavaScript iebūvēti objekti, to īpašības un metodes no MDNURL](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects)
+5. [JavaScript Tutorial no w3shcools](https://www.w3schools.com/js/);
+6. [JavaScript Tutorial no JavaScriptTutorial.netURL](https://www.javascripttutorial.net);
+7. [JavaScript uzdevumi no hackerrank](https://www.hackerrank.com/contests/7days-javascript/challenges);
+8. [Grāmatas no GoalKicker](https://goalkicker.com/);
+9. [HTML valodas standarts](https://html.spec.whatwg.org/);
+10. [JavaScript valodas standarts](https://262.ecma-international.org/12.0/);
+11. [Latvijas Nacionālais terminoloģijas portāls](https://termini.gov.lv/);
+12. [20+ Web Projects With Vanilla JavaScript](https://vanillawebprojects.com/);
+13. [Projects You Can Build With Vanilla JavaScript](https://medium.com/swlh/projects-you-can-build-with-vanilla-javascript-e52c3e00f25c);
+14. [ ]
