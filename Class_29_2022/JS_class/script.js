8d9e84c446de097b79302a1f918ae462896cdf9e
Kyrtra <ilze_senk@inbox.lv>
2022-09-21 19:05:47 +0300

    21/09/2022

--- Class_29_2022/JS_class/script.js
+++ Class_29_2022/JS_class/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿//Viens teksta lauks + poga ("Pievienot jaunu ievad lauku").
+//Lauks ar nosaukumu
+//Jautājums - kā to lauku sauc?
+
+//https://toolsnull.com/code-solution/how-to-create-dynamic-form-fields-in-javascript
+  
+let btn = document.querySelector('#btn');
+
+function display() {
+    
+    let inputboxname_ = prompt("Ievadiet Input Box nosaukumu");
+    var input = document.createElement("input");
+    input.name=inputboxname_;
+    input.value = inputboxname_;
+    input.type = 'text';
+    menu.appendChild(input);
+    let poga = document.createElement("button");
+    poga.innerHTML='delete';
+    menu.appendChild(poga);
+
+ 
+}
+btn.addEventListener('click',display);
---
+++ funkcija/main.js
@@ -0,0 +1,9 @@
+window.name = "original";
+function OpenNewWindow(){
+window.open(
+  "http://127.0.0.1:5500/funkcija/sign_up/sign_up.html",
+  //"https://www.w3schools.com/js/js_object_prototypes.asp",
+  "test",
+  "height=500,width=500"
+);
+}
